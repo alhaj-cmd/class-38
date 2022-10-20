0909f159cd49ab6ad8c2c7c029965b0103720747
github_username <you@domain.com>
2022-10-20 16:22:26 +0600

    first commit

---
+++ js/alert.js
@@ -0,0 +1,15 @@
+function nameAlter(){
+   confirm('call me')
+    
+}
+
+function nameConfirm(){
+    const num =Math.random()*10;
+    if(num < 5){
+   alert('Continue your studay')
+    }
+}
+
+function namePromt(){
+    prompt('name validation')
+}

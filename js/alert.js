d29e5ad091bb22b5972b49cf3730348d9541beec
github_username <you@domain.com>
2022-10-21 17:38:08 +0600

    complete

--- js/alert.js
+++ js/alert.js
@@ -1,15 +1,50 @@
-function nameAlter(){
-   confirm('call me')
-    
+
+const getInputvalueByid = id =>{
+    const inputField = document.getElementById(id);
+    const inputValue = inputField.value;
+    inputField.value = '';
+    return inputValue;
 }
 
-function nameConfirm(){
-    const num =Math.random()*10;
-    if(num < 5){
-   alert('Continue your studay')
+const addProduct = () =>{
+    const product = getInputvalueByid('inputField');
+    const quentity = getInputvalueByid('inputData');
+    addProductToDisplay(product,quentity);
+    // localStorage.setItem(product,quentity);
+    saveItemlocalstore(product,quentity);
+}
+
+const getShopinglocalstore =()=>{
+    let saveCart =localStorage.getItem('cart');
+    let cart ={};
+    if(saveCart){
+        cart = JSON.parse(saveCart);
+    }
+    return cart;
+}
+
+const saveItemlocalstore = (product,quentity) =>{
+    const cart =getShopinglocalstore();
+    cart[product] = quentity;
+    const cartStringiFied = JSON.stringify(cart)
+
+    localStorage.setItem('cart',cartStringiFied);
+
+}
+
+const addProductToDisplay =(product, quentity) =>{
+    const productContainer =document.getElementById('product-container');
+    const li = document.createElement('li');
+    li.innerHTML = `${product} : ${quentity}`;
+    productContainer.appendChild(li);
+}
+
+const displaysaveCartProduct = () =>{
+    const cart = getShopinglocalstore();
+    for(const product in cart){
+        const quentity = cart[product];
+        console.log(product, quentity);
+        addProductToDisplay(product, quentity);
     }
 }
-
-function namePromt(){
-    prompt('name validation')
-}
+displaysaveCartProduct()
